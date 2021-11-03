c0cb4ba1b2579ccd84bc45e858a1a72cb65905a9
Rohan Patel <46540609+rohanhpatel@users.noreply.github.com>
2021-11-03 17:31:34 -0400

    Working locally with create, get b.txt, and quit
    Doesn't kill processes though, pain

--- main.erl
+++ main.erl
@@ -1,7 +1,8 @@
--module(helloworld).
-
+-module(main).
 % main functions
--export([client/1, combineFiles/2, getServers/4, partition/5, split/2, separateFiles/3, file_server/2, dir_service/2, start_file_server/1, start_dir_service/0, get/2, create/2, quit/1]).
+% main functions
+-export([destroy_all_files/2, client/2, client_get/2, contact_servers/2, combineFiles/2, getServers/4, partition/5, split/2, separateFiles/3, file_server/2,
+		 dir_service/2, start_file_server/1, start_dir_service/0, get/2, create/2, quit/1]).
 
 -import(util, [readFile/1,get_all_lines/1,saveFile/2]).
 
@@ -13,31 +14,30 @@
 % grading will be those below
 
 %split into 64 character strings
-
-split(Fullfile, List) ->
-	Length = length(Fullfile),
+split(String, List) ->
 	if
-		Length < 64 -> 
-			lists:append(List,[Fullfile]);
-		true -> 
-			First64 = string:slice(Fullfile,0,64),
+		length(String) < 64 ->
+			lists:append(List,[String]);
+		true ->
+			io:format("~s~n", [integer_to_list(length(String))]),
+			First64 = string:slice(String, 0, 64),
 			AppendList = lists:append(List,[First64]),
-			split(lists:prefix(Fullfile, First64), AppendList)
+			split(string:prefix(String, First64), AppendList)
 	end.
 
 % Map each filename to server
-partition(ServerList, FileMap, SplitFile, Filenames, Index) -> 
-	case {Filenames} of
+partition(ServerList, FileMap, SplitFile, Filenames, Index) ->
+	case Filenames of
 		[] ->
 			FileMap;
-		[ Head| Tail ] -> 
+		[ Head| Tail ] ->
 			if
 				Index > length(ServerList) ->
 					NewMap = maps:put(Head, lists:nth(1,ServerList), FileMap),
 					[HeadString | Rest] = SplitFile,
 					lists:nth(1,ServerList) ! {create_file, HeadString, Head},
 					partition(ServerList, NewMap, Rest, Tail, 2);
-				true -> 
+				true ->
 					NewMap = maps:put(Head, lists:nth(Index,ServerList), FileMap),
 					[HeadString | Rest] = SplitFile,
 					lists:nth(Index,ServerList) ! {create_file, HeadString, Head},
@@ -45,130 +45,156 @@ partition(ServerList, FileMap, SplitFile, Filenames, Index) ->
 			end
 	end.
 
-%create filenames from 64 character 
-create_files(SplitFile,Filename, OutputList) -> 
+%create filenames from 64 character
+create_files(SplitFile, Filename, OutputList) ->
 	case SplitFile of
 		[] ->
 			OutputList;
-		[Head | Tail] -> 
+		[_ | Tail] ->
 			OnlyFileName =  lists:last(string:split(Filename, "/", trailing)),
 			WithoutTxt = lists:nth(1, string:split(OnlyFileName, ".", trailing)),
-			NewList = lists:append(OutputList, [WithoutTxt ++ "_" ++  (integer_to_list(length(OutputList) + 1))]),
+			NewList = lists:append(OutputList, [WithoutTxt ++ "_" ++ integer_to_list(length(OutputList) + 1)]),
 			create_files(Tail, Filename, NewList)
 	end.
 
 %separatesFile into 64 characters each, and maps with Servers
-separateFiles(ServerList, FileMap, Filename) -> 
+separateFiles(ServerList, FileMap, Filename) ->
 	Fullfile = util:readFile("./input/" ++ Filename),
 	SplitFile = split(Fullfile, []),
 	Filenames = create_files(SplitFile,Filename, []),
 	partition(ServerList, FileMap, SplitFile, Filenames, 1).
 
 
-getServers(Filepattern, FileMap, Servers, Index) -> 
-	Filename = Filepattern ++ integer_to_list(Index),
+getServers(Filepattern, FileMap, Servers, Index) ->
+	Filename = Filepattern ++ "_" ++ integer_to_list(Index),
 	MatchedServer = maps:get(Filename, FileMap, "CANT_FIND"),
 	case MatchedServer of
-		"CANT_FIND" -> 
+		"CANT_FIND" ->
 			Servers;
-		X -> 
+		X ->
 			NewServers = lists:append(Servers, [{X, Filename}]),
 			getServers(Filepattern, FileMap, NewServers, Index + 1)
 	end.
 
-
-dir_service(ServerList, FileMap) -> 
-	
-	receive 
-	    {create_fserver, ServerPID} -> UpdatedList = lists:append(ServerList, [ServerPID]), 
-	    							    lists:sort(UpdatedList),
-	    							    ServerPID ! {make_dir, integer_to_list(length(UpdatedList))},
-	    						   	    dir_service(UpdatedList,FileMap);
-	    quit -> UpdatedList = lists:map(fun(Fserver) -> {Fserver ! quit} end, ServerList),
-	    		dir_service(UpdatedList,FileMap);
-	    {create_file, Filename} ->  NewMap = separateFiles(ServerList, FileMap, Filename),
-	    							dir_service(ServerList, NewMap);
-	    {get_file, Filename, ClientPID} -> WithoutTxt = lists:nth(1, string:split(Filename, ".", trailing)),
-								ClientPID ! {receive_servers, getServers(WithoutTxt, FileMap, [], 1)},
-								dir_service(ServerList, FileMap)
+% requests file information from the Directory Service (DirUAL) on File
+% then requests file parts from the locations retrieved from Dir Service
+% then combines the file and saves to downloads folder
+combineFiles(FileStringTup, OutputString) ->
+	case FileStringTup of
+		[] ->
+			OutputString;
+		[{_, Content} | Tail] ->
+			NewOutput = OutputString ++ Content,
+			combineFiles(Tail, NewOutput)
 	end.
 
-file_server(DirUAL, Path) -> 
-	receive 
+contact_servers(ServersPlusFile, ClientPID) ->
+	case ServersPlusFile of
+		[] ->
+			io:format("~s~n", ["client download"]),
+			ClientPID ! download_file;
+		[{Server, Filename} | Tail] ->
+			io:format("~s~n", [Filename]),
+			Server ! {get_file, Filename, ClientPID},
+			contact_servers(Tail, ClientPID)
+	end.
+
+%helper functions for quitting servers
+destroy_all_files(_, []) ->
+	ok;
+destroy_all_files(FileMap, [Filename | Rest]) ->
+	Server = maps:get(Filename, FileMap),
+	Server ! {del_file, Filename},
+	destroy_all_files(FileMap, Rest).
+
+dir_service(ServerList, FileMap) ->
+	receive
+	    {create_fserver, ServerPID} -> UpdatedList = lists:append(ServerList, [ServerPID]),
+	    							    lists:sort(UpdatedList),
+										io:format("~w~n", [UpdatedList]),
+	    							    ServerPID ! {make_dir, integer_to_list(length(UpdatedList))},
+	    						   	    dir_service(UpdatedList, FileMap);
+	    {create_file, Filename} ->  NewMap = separateFiles(ServerList, FileMap, Filename),
+	    							dir_service(ServerList, NewMap);
+		{get_file, Filename, ClientPID} -> WithoutTxt = lists:nth(1, string:split(Filename, ".", trailing)),
+								ClientPID ! {receive_servers, getServers(WithoutTxt, FileMap, [], 1)},
+								dir_service(ServerList, FileMap)
+		after 200 ->
+			receive
+				quit -> Keys = maps:keys(FileMap),
+						destroy_all_files(FileMap, Keys),
+						lists:foreach(fun(Server) ->
+											Server ! del_server
+									  end, ServerList),
+						exit(kill)
+			end
+	end.
+
+file_server(DirUAL, Path) ->
+	receive
 		{make_dir, Number} -> file:make_dir("./servers/fs" ++ Number),
-							  NewPath = "./servers/fs" ++ Number ++ "/",
-						      file_server(DirUAL,NewPath);
-		{create_file, Contents, Filename} -> util:saveFile(Path ++ Filename ++ ".txt", Contents),
-										     file_server(DirUAL, Path);
-		{get_file, Filename, ClientPID} -> StringContent = util:readFile(Path ++ Filename ++ ".txt"),
-										   ClientPID ! {retrieve_content, StringContent}
+							  NewPath = "./servers/fs" ++ Number,
+							  file_server(DirUAL, NewPath);
+		{create_file, Contents, Filename} -> util:saveFile(Path ++ "/" ++ Filename ++ ".txt", Contents),
+											 file_server(DirUAL, Path);
+		{get_file, Filename, ClientPID} -> StringContent = util:readFile(Path ++ "/" ++ Filename ++ ".txt"),
+										   io:format("~s~n", [StringContent]),
+										   ClientPID ! {get_content, {Filename, StringContent}},
+										   file_server(DirUAL, Path);
+		{del_file, Filename} -> file:delete(Path ++ "/" ++ Filename ++ ".txt"),
+							file_server(DirUAL, Path);
+		del_server -> file:del_dir(Path),
+					  exit(kill)
 	end.
 
 % when starting the Directory Service and File Servers, you will need
 % to register a process name and spawn a process in another node
 
 % starts a directory service
-start_dir_service() -> 
-	register(direc, spawn(helloworld, dir_service, [ [] ,maps:new()])).
+start_dir_service() ->
+	register(direc, spawn(main, dir_service, [[], maps:new()])).
 	% CODE THIS
 
 
 % starts a file server with the UAL of the Directory Service
 %contacts dir service, dir service updates list of known file servers, alphabetically sorted
-start_file_server(DirUAL) -> 
-	F = spawn(helloworld, file_server, [DirUAL, ""]),
+start_file_server(DirUAL) ->
+	F = spawn(main, file_server, [DirUAL, ""]),
 	{direc, DirUAL} ! {create_fserver, F}.
-
 	% CODE THIS
-
-% requests file information from the Directory Service (DirUAL) on File
-% then requests file parts from the locations retrieved from Dir Service
-% then combines the file and saves to downloads folder
-
-combineFiles(StringContent, OutputString) ->
-	case StringContent of
-		[] ->
-			OutputString;
-		[Head | Tail] ->
-			NewOutput = OutputString ++ Head,
-			combineFiles(Tail, NewOutput)
-	end.
-
-contact_servers(ServersPlusFile) -> 
-	case ServersPlusFile of
-		[] ->
-			nothing;
-		[{Servers, Filename} | Tail] ->
-			Servers ! {get_file, Filename},
-			contact_servers(Tail)
-	end.
-
-	
-client(Content) ->
-	receive 
-		{get_file, File, DirUAL} -> {direc, DirUAL} ! {get_file, File, self()},
-									client(Content);
-		{receive_servers, ServersPlusFile} ->  contact_servers(ServersPlusFile),
-											   client(Content);
-		{retrieve_content, StringContent} -> NewContent = Content ++ [StringContent], 
-											 client(NewContent);
-		download_file -> FullString = combineFiles(Content, [])
-
-	end.
-
-
 
 %
-get(DirUAL, File) -> 
+get(DirUAL, File) ->
 	OnlyFileName =  lists:last(string:split(File, "/", trailing)),
-	Client = spawn(helloworld, client, []),
-	Client ! {get_file, File, DirUAL},
-	Client ! {download_file}.
-	
-	%FullString = combineFiles(ServersPlusFile, ""),
-	%util:saveFile("./downloads/" ++ OnlyFileName).
+	Client = spawn(main, client, [OnlyFileName, []]),
+	Client ! {get_file, DirUAL}.
 
-	% CODE THIS
+
+%used for getting files
+client(Filename, Content) ->
+	receive
+		{get_file, DirUAL} -> {direc, DirUAL} ! {get_file, Filename, self()},
+							  client(Filename, Content);
+		{receive_servers, ServersPlusFile} -> CG_PID = spawn(main, client_get, [Filename, Content]),
+											  contact_servers(ServersPlusFile, CG_PID)
+	end.
+
+client_get(Filename, Content) ->
+	receive
+		{get_content, StringContent} -> NewContent = Content ++ [StringContent],
+										 io:format("Content is ~p and NewContent is ~p~n", [Content, NewContent]),
+										 client_get(Filename, NewContent)
+		after 100 ->
+			receive
+				{get_content, StringContent} -> NewContent = Content ++ [StringContent],
+												 io:format("Content is ~p and NewContent is ~p~n", [Content, NewContent]),
+												 client_get(Filename, NewContent);
+				download_file -> 	SortedContent = lists:keysort(1, Content),
+									FileString = combineFiles(SortedContent, ""),
+									io:format("Content: ~p, saving now~n", [FileString]),
+									util:saveFile("./downloads/" ++ Filename, FileString)
+			end
+	end.
 
 % gives Directory Service (DirUAL) the name/contents of File to create
 % splits file into chunks of 64 characters, sends each chunk to a known file server, following alphabetical
@@ -178,11 +204,7 @@ create(DirUAL, File) ->
 	% CODE THIS
 
 % sends shutdown message to the Directory Service (DirUAL)
-% ends all file servers 
+% ends all file servers
 quit(DirUAL) ->
-	pass.
+	{direc, DirUAL} ! quit.
 	% CODE THIS
-
-
-
-
